f2b407aa56ff7e1144d1fcc810cd5a6dc743614b
kstephen3@gmail.com <kstephen3@gmail.com>
2026-07-12 01:16:38 +0300

    chore: remove accidental image.push.sh helper script

--- image.push.sh
+++
@@ -1,4 +0,0 @@
-docker tag driving_school_crm_2_backend:latest kstephen3/crm_backend:latest && \
-docker push kstephen3/crm_backend:latest && \
-docker tag driving_school_crm_2_frontend:latest kstephen3/crm_frontend:latest && \
-docker push kstephen3/crm_frontend:latest
